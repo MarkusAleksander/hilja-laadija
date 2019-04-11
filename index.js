1a9436839dbfe763ce6a0236f2b0e9905fdfe4fe
dev <dev@C02NR3MJF8J4.local>
2019-04-11 13:39:55 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,92 @@
+/*
+Lazy Loader Module (hilja laadija - Late Loader)
+Developed by Mark Holden
+Version 1.0.0
+*/
+var hiljaLaadija = (function hiljaLaadija() {
+    var _data = {
+        placeholderTarget: '#lazyloaderPlaceholder',
+        placeholderTargetEl: null,
+        serverLocation: null,
+        imageArr: null,
+        offset: 500,
+        negOffset: -500,
+        scrolling: false,
+        high_bound: window.innerHeight
+    };
+    function _test(test, message) {
+        if (test) {
+            console.log(message);
+            return true;
+        }
+        return false;
+    }
+    function _loadImages() {
+        _data.high_bound = window.innerHeight + _data.offset;
+        for (var i = 0; i < _data.imageArr.length; i++) {
+            var bounds = _data.imageArr[i].getBoundingClientRect();
+            if ((bounds.top < _data.high_bound) && ((bounds.top + bounds.height) > _data.negOffset)) {
+                var imgSrc = _data.imageArr[i].getAttribute('data-lazy-src');
+                if (imgSrc.indexOf(_data.serverLocation) == -1) {
+                    imgSrc = _data.serverLocation + imgSrc;
+                }
+                if (_data.imageArr[i].tagName == "IMG") {
+                    _data.imageArr[i].setAttribute('src', imgSrc);
+                }
+                else {
+                    _data.imageArr[i].style.backgroundImage = "url('" + imgSrc + "')";
+                }
+                _data.imageArr[i].classList.add('lazy-loaded');
+            }
+        }
+    }
+    function _run() {
+        // * Get lazy loadable images when document has loaded
+        _data.imageArr = document.querySelectorAll('[data-lazy-src]:not(.lazy-loaded)');
+        if (_test(_data.imageArr.length == 0, 'No lazy loadable images found')) {
+            return;
+        }
+        _loadImages();
+        // * Attach window scroll event
+        window.addEventListener('scroll', function onWindowScroll() {
+            _data.scrolling = true;
+        });
+        // * Run through images and update SRC
+        window.setInterval(function _checkImages() {
+            if (_data.scrolling) {
+                _data.scrolling = false;
+                _data.imageArr = document.querySelectorAll('[data-lazy-src]:not(.lazy-loaded)');
+                _loadImages();
+            }
+        }, 150);
+    }
+    function _init() {
+        // * Initialise variables before running
+        console.log('Beginning lazy load...');
+        _data.placeholderTargetEl = document.querySelector(_data.placeholderTarget);
+        // * Requires placeholder image
+        if (_test(!_data.placeholderTargetEl, 'Lazy Load Placeholder not found!')) {
+            return;
+        }
+        // * Get path
+        var path = _data.placeholderTargetEl.getAttribute('src');
+        _data.serverLocation = path.slice(0, path.indexOf('/repo'));
+    }
+    return {
+        init: _init,
+        run: _run
+    };
+})();
+hiljaLaadija.init();
+(function initAction() {
+    function ready(fn) { if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        fn();
+    }
+    else {
+        document.addEventListener('DOMContentLoaded', fn);
+    } }
+    ready(function run() {
+        // Now do stuff...
+        hiljaLaadija.run();
+    });
+})();
